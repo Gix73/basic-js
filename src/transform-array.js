ed1209e984c662686d46c4954b95f9552cd5267b
Gix73 <gix73@yandex.ru>
2021-02-11 20:57:59 +0300

    feat: add new transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const control_seq = [`--discard-next`, `--double-prev`, `--double-next`, `--discard-prev`];
+  if(Array.isArray(arr) == false){
+    throw new Error;
+  };
+
+  return arr.reduce((previousValue, currentItem, index) => {
+    if (control_seq.includes(currentItem)) {
+      return previousValue;
+    }
+
+    if (arr[index - 1] == `--discard-next`) {
+      return previousValue;
+    }
+
+    if (arr[index - 1] == `--double-next`) {
+      previousValue.push(currentItem, currentItem);
+    } else {
+      previousValue.push(currentItem);
+    }
+
+    if (arr[index + 1] == `--discard-prev`) {
+      previousValue.splice(previousValue.length - 1, 1)
+    }
+
+    if (arr[index + 1] == `--double-prev`) {
+      previousValue.push(currentItem)
+    }
+    return previousValue;
+  }, []);
 };
