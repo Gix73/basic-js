b4013b309e730ef198370175890538d7ea722d1e
Gix73 <gix73@yandex.ru>
2021-02-21 23:05:46 +0300

    feat: add extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+//str add addRep addSep
+let str1=str;
+str = "";
+let repeatTimes = options.repeatTimes;
+let separator = options.separator;
+let addition = options.addition;
+let additionRepeatTimes = options.additionRepeatTimes;
+let additionSeparator = options.additionSeparator;
+if (repeatTimes === undefined){
+  repeatTimes = 1;
+}
+
+if (separator === undefined){
+  separator = "+";
+}
+
+if (addition === undefined){
+  addition = "";
+}
+
+if (additionRepeatTimes === undefined){
+  additionRepeatTimes = 1;
+}
+
+if (additionSeparator === undefined){
+  additionSeparator = "|";
+}
+for (let i = 0; i < additionRepeatTimes; i++){
+  if (i == additionRepeatTimes-1){
+    str1 += addition + separator;
+  }else{
+    str1 += addition + additionSeparator;
+  }
+}
+for (let i = 0; i < repeatTimes; i++){
+  if(i == repeatTimes - 1){
+    str += str1.substr(0, str1.length-separator.length);
+  }else{
+    str += str1;
+  }
+
+}
+return str;
 };
   
